c55300076301fc685d27984e0516aeaf78792768
Joaquin <joaqobianchi13@gmail.com>
2023-09-27 14:22:19 -0300

    add prodia API

--- src/app/page.js
+++ src/app/page.js
@@ -4,29 +4,60 @@ import { useState } from "react";
 export default function Home() {
   const [prompt, setPrompt] = useState("");
   const [imageUrl, setImageUrl] = useState("");
-
   const handleGenerateImage = async () => {
+    const jobId = await fetchJobId();
+
+    if (jobId) {
+      const image = await fetchImage(jobId);
+
+      if (image) {
+        setImageUrl(image);
+      }
+    }
+  };
+  const fetchJobId = async () => {
+    try {
+      // Realiza la solicitud para obtener un jobId
+      const options = {
+        method: "POST",
+        headers: {
+          accept: "application/json",
+          "content-type": "application/json",
+          "X-Prodia-Key": "e8346eb2-6187-4748-a42f-7241580ee1f1",
+        },
+        body: JSON.stringify({ prompt: prompt }), // Aquí usas la variable 'prompt'
+      };
+
+      // Realiza la solicitud con 'fetch' y espera la respuesta
+      const response = await fetch(
+        "https://api.prodia.com/v1/sd/generate",
+        options
+      );
+
+      if (response.status === 200) {
+        const responseData = await response.json();
+        console.log(responseData);
+      } else {
+        console.error("Error en la solicitud:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error en la solicitud:", error);
+    }
+  };
+
+  const fetchImage = async (jobId) => {
     const options = {
-      method: "POST",
+      method: "GET",
       headers: {
         accept: "application/json",
-        "content-type": "application/json",
+        "X-Prodia-Key": "e8346eb2-6187-4748-a42f-7241580ee1f1",
       },
-      body: JSON.stringify({ prompt: prompt }),
     };
 
-    try {
-      const response = await fetch("/api/route", options);
-      if (!response.ok) {
-        throw new Error("Error al obtener la imagen");
-      }
-
-      const responseData = await response.json();
-      const imageUrl = responseData.imageUrl;
-      setImageUrl(imageUrl);
-    } catch (error) {
-      console.error(error);
-    }
+    fetch(`https://api.prodia.com/v1/job/${jobId}`, options)
+      .then((response) => response.json())
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err));
   };
 
   return (
